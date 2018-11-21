65d5bc1f01ca917d03e56088e6b009ff61f631a9
Aidas Adomkus <aidas@jug.lt>
2018-11-21 16:27:38 +0200

    Somewhere updated CompareUtils tests

--- src/test/java/com/triopin/commons/util/CompareUtilsTest.java
+++ src/test/java/com/triopin/commons/util/CompareUtilsTest.java
@@ -1,6 +1,17 @@
 package com.triopin.commons.util;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class CompareUtilsTest {
 
+	@Test
+	@SuppressWarnings("deprecation")
+	public void compareComparable() {
+		String v1 = "A";
+		String v2 = "B";
+		assertEquals(CompareUtils.compare(v1, v2),
+				org.apache.commons.lang.ObjectUtils.compare(v1, v2));
+	}
 }
